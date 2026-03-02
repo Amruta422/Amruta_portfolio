95330fb05deb2d1297e3412da7770fd8a6f81652
Amruta422 <amrutay412@gmail.com>
2026-03-02 13:24:34 +0530

    Fixed CSS import

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Replace 'portifolio' with your exact GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/Amruta_portfolio/',  // <-- MUST match your GitHub repo name exactly
-})
+  base: '/Amruta_portfolio/', // MUST match your GitHub repo name exactly
+});
